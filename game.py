5777703de5fe6234609f8ea2a36c75b212ca51b5
goose1675 <agamsabharwal@gmail.com>
2025-04-19 16:39:49 -0400

    I added comments and fixed an error where it didnt stop the game when the player busts

--- game.py
+++ game.py
@@ -8,15 +8,19 @@ num = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "ki
 cards = []
 for t in types:
     for n in num:
-        cards.append(n + "of" + t)
+        cards.append(n + "of" + t) 
+
+#This creates a deck of cards. The cards are lited as "ace of hearts", "2 of hearts", "3 of hearts"... and so on for 52 cards 
+# Types stores the type of card, while num stores its number/value  
 
 
-def add_card_points(card, to_dealer=True):
+def add_card_points(card, to_dealer=True): # A function that when called, gives the player or dealer a score of points depending on the cards in their hand
+
     global dealer_point, playerpoint
 
     if "ace" in card:
         if to_dealer: 
-            if dealer_point + 11 <= 21:
+            if dealer_point + 11 <= 21: #An if statement that decides if the ace should be counted as 10 point or 1 point, depending on the player or dealer's other cards. 
                 points = 11 
             else:
                 points = 1
@@ -32,9 +36,9 @@ def add_card_points(card, to_dealer=True):
         points = int(card.split("of")[0])
     
     if to_dealer:
-        dealer_point += points
+        dealer_point += points # If called to give a dealer points, to_dealer stays as a default: True
     else:
-        playerpoint += points
+        playerpoint += points# If called to give the player points, to_dealer is False,
 
         
     
@@ -48,39 +52,39 @@ def add_card_points(card, to_dealer=True):
 
 
 
-balance = 1000
+balance = 1000 #A set balance that every player starts with 
 winnings = 0
 running = True
 
-while running: 
-    playerpoint = 0 
-    dealer_point=0
+while running: # The game loop 
+    playerpoint = 0 # Variable for the total points the player recieves based off their cards
+    dealer_point=0  # Variable for the total points the dealer recieves based off their cards 
     print("Your balance is at $", balance,  ".")
     bet = int(input("How much would you like to bet? "))
     while bet >= balance:
-        print("You must bet less than or equal to your balance!")
+        print("You must bet less than or equal to your balance!") #Insures that the bet is not more than the balance
         bet = int(input("How much would you like to bet? "))
 
     balance = balance - bet
-    print("Ok, you have placed your bet!")
+    print("Ok, you have placed your bet!") 
 
 
     
-    playercard_1 = random.choice(cards)
+    playercard_1 = random.choice(cards) # The player's first card is pulled and then removed from the deck so it cant be pulled again by anyone else.
     cards.remove(playercard_1)
     
-    dealercard_1 = random.choice(cards)
+    dealercard_1 = random.choice(cards)# The dealer's first card is pulled and then removed from the deck so it cant be pulled again by anyone else.
     cards.remove(dealercard_1)
   
 
    
     print("The player's first card is ", playercard_1)
-    add_card_points(playercard_1, to_dealer=False)
+    add_card_points(playercard_1, to_dealer=False) #The players card is printed and a value is assigned
     print(playerpoint)
 
 
     print("The dealer's first card is ", dealercard_1)
-    add_card_points(dealercard_1, to_dealer=True)
+    add_card_points(dealercard_1, to_dealer=True)  #The dealers card is printed and a value is assigned
     print(dealer_point)
 
 
@@ -91,13 +95,13 @@ while running:
     add_card_points(playercard_2, to_dealer=False)
     print(playerpoint)
 
-    if playerpoint == 21:
+    if playerpoint == 21: #An auto win condition that shows that if the player hits 21, then they win 
         print("Auto win!")
         winnings = bet * 1.5
         print("You win ", winnings)
         balance += winnings
         print("Balance: ", balance)
-        break
+        
 
     elif playerpoint < 21:
         choice = input("Would you like to hit (draw a new card) (h) or stand (keep your current hand) (s)? ").lower()
@@ -136,7 +140,7 @@ while running:
     
 
     
-    while dealer_point <= 16:
+    while dealer_point <= 16 and playerpoint < 21:
         dealercard_extra = random.choice(cards)
         cards.remove(dealercard_extra)
         print("Dealer draws:", dealercard_extra)
@@ -147,7 +151,8 @@ while running:
 
     if playerpoint > 21:
         print("Bust! Dealer wins.")
-    elif dealer_point > 21:
+        break
+    elif dealer_point > 21 and playerpoint <  21:
         print("Dealer busts! You win!")
         balance += bet * 2
     elif playerpoint > dealer_point:
@@ -155,6 +160,7 @@ while running:
         balance += bet * 2
     elif playerpoint < dealer_point:
         print("Dealer wins.")
+    
     else:
         print("It's a tie!")
         balance += bet
